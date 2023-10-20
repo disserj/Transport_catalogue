04f9d621c4462223e3a434a2b047c4231d0e1ca1
disserj <116752351+disserj@users.noreply.github.com>
2023-10-20 11:28:09 +0500

    Update stat_reader.h

--- transport-catalogue/stat_reader.h
+++ transport-catalogue/stat_reader.h
@@ -1,1 +1,17 @@
-// место для вашего кода
+#pragma once
+#include "transport_catalogue.h"
+#include "input_reader.h"
+#include <iostream>
+#include <vector>
+#include <string>
+#include <tuple>
+
+namespace transport{
+
+void Response(std::ostream& out,Request_Queue& request, TransportCatalogue& catalogue);
+
+void BusResponse(std::ostream& out, std::pair<std::string,bool> bus,TransportCatalogue& catalogue);
+
+void StopResponse(std::ostream& out, std::pair<std::string,bool> stop,TransportCatalogue& catalogue);
+    
+} // exit transport    
