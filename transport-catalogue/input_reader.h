65877b0619009aa3e4edca928fc319a7f2c7b24e
disserj <116752351+disserj@users.noreply.github.com>
2023-10-20 11:33:43 +0500

    Update input_reader.h

--- transport-catalogue/input_reader.h
+++ transport-catalogue/input_reader.h
@@ -1,1 +1,26 @@
-// место для вашего кода
+#pragma once
+#include "geo.h"
+#include "transport_catalogue.h"
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <utility>
+
+namespace transport{
+
+struct Request_Queue{
+    std::vector<std::string>ToFill;
+    std::vector<std::string>ToResponse;
+};
+
+
+void Read(std::istream& in, Request_Queue& request);
+void FillCatalogue(Request_Queue& request,TransportCatalogue& catalogue);
+std::pair<std::vector<std::string>, bool> ReadRoute(std::string& str);
+
+std::pair<std::string,std::string> FillStops(std::string& str, TransportCatalogue& catalogue);
+void FillBuses(std::string& str, TransportCatalogue& catalogue);
+void FillDistances(std::string& name, std::string& str, TransportCatalogue& catalogue);
+  
+}// exit transport    
