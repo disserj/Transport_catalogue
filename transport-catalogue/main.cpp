c1660748780ee406f5c4822b1305271f2442e053
disserj <116752351+disserj@users.noreply.github.com>
2023-10-20 11:32:32 +0500

    Update main.cpp

--- transport-catalogue/main.cpp
+++ transport-catalogue/main.cpp
@@ -1,1 +1,14 @@
-// место для вашего кода
+#include "transport_catalogue.h"
+#include "input_reader.h"
+#include "stat_reader.h"
+#include <iostream>
+
+using namespace transport;
+
+int main() {
+    TransportCatalogue catalogue;
+    Request_Queue request;
+    Read(std::cin, request);
+    FillCatalogue(request, catalogue);
+    Response(std::cout, request, catalogue);
+}
