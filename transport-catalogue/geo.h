a73d70ca6c912289b4b44844b6cf19cc500bac44
disserj <116752351+disserj@users.noreply.github.com>
2023-11-27 17:35:29 +0500

    Update geo.h

--- transport-catalogue/geo.h
+++ transport-catalogue/geo.h
@@ -17,14 +17,5 @@ struct Coordinates {
     }
 };
 
-inline double ComputeDistance(Coordinates from, Coordinates to) {
-    using namespace std;
-    if (from == to) {
-        return 0;
-    }
-    static const double dr = 3.1415926535 / 180.;
-    return acos(sin(from.lat * dr) * sin(to.lat * dr)
-                + cos(from.lat * dr) * cos(to.lat * dr) * cos(abs(from.lng - to.lng) * dr))
-        * RADIUS;
-}
+ double ComputeDistance(Coordinates from, Coordinates to);
 }//exit geo
