a6ba64ee8c7107768149417ae1a7adb42ba6496b
disserj <116752351+disserj@users.noreply.github.com>
2023-11-28 12:06:38 +0500

    Update geo.cpp

--- transport_catalogue/geo.cpp
+++ transport_catalogue/geo.cpp
@@ -13,7 +13,7 @@ double ComputeDistance(Coordinates from, Coordinates to) {
     static const double dr = 3.1415926535 / 180.;
     return acos(sin(from.lat * dr) * sin(to.lat * dr)
                 + cos(from.lat * dr) * cos(to.lat * dr) * cos(abs(from.lng - to.lng) * dr))
-        * RADIUS;
+        * EARTH_RADIUS;
 }
 
 }  // namespace geo
