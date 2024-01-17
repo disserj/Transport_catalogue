79302479febfc8239b43fab4abe0abf77bdbf65e
disserj <116752351+disserj@users.noreply.github.com>
2024-01-17 18:38:45 +0500

    Update transport_router.h

--- transport-catalogue/transport_router.h
+++ transport-catalogue/transport_router.h
@@ -68,54 +68,54 @@ void RouteManager::BuildGraph(TransportCatalogue&& catalogue) {
     std::unordered_map<std::string_view, graph::VertexId> stop_ids;
     graph::VertexId vertex_id = 0;
     
-    std::for_each(stop_catalogue.begin(),stop_catalogue.end(),
-    [this, &stop_ids, &stops_graph, &vertex_id](const auto& name_and_data){
-    stop_ids[name_and_data.second.get()->name] = vertex_id;
-    stops_graph.AddEdge({
-                vertex_id,
-                ++vertex_id,
-                EdgeData({name_and_data.second.get()->name, 0, wait_time})
-            });
-        ++vertex_id;
+    std::for_each(stop_catalogue.begin(),stop_catalogue.end(), 
+	    [this, &stop_ids, &stops_graph, &vertex_id](const auto& name_and_data){
+		    stop_ids[name_and_data.second.get()->name] = vertex_id;
+		    stops_graph.AddEdge({
+		                vertex_id,
+		                ++vertex_id,
+		                EdgeData({name_and_data.second.get()->name, 0, wait_time})
+		            	});
+		     ++vertex_id;
     });
     stops_to_points = std::move(stop_ids);
 
     std::for_each(bus_catalogue.begin(), bus_catalogue.end(),
-        [this, &stops_graph, &catalogue](const auto& num_and_data) {
-            const auto& bus_data = num_and_data.second;
-            const auto& stops = bus_data.get()->stop_ptrs;
-            size_t stops_count = stops.size();
-            for (size_t i = 0; i < stops_count; ++i) {
-                for (size_t j = i + 1; j < stops_count; ++j) {
-                
-                    const std::shared_ptr<Stop> from = stops[i];
-                    const std::shared_ptr<Stop> to = stops[j];
-                    
-                    int dist_sum = 0;
-                    int dist_sum_inverse = 0;
-                    
-                    for (size_t x = i + 1; x <= j; ++x) {
-                        dist_sum += catalogue.GetDistance(stops[x - 1].get(), stops[x].get());
-                        dist_sum_inverse += catalogue.GetDistance(stops[x].get(), stops[x - 1].get());
-                      
-                    }
-                    
-                    stops_graph.AddEdge({
-                    stops_to_points.at(from.get()->name) + 1,
-                    stops_to_points.at(to.get()->name),
-                    
-                    EdgeData({bus_data.get()->num, (int)(j-i), (double)(dist_sum) / (velocity * KM_H)})
-                    });    
-                    if (bus_data.get()->type == RouteType::LINEAR) {
-                        stops_graph.AddEdge({
-                        stops_to_points.at(to.get()->name) + 1,
-                        stops_to_points.at(from.get()->name),
-                        
-                        EdgeData({bus_data.get()->num, (int)(j-i), (double)(dist_sum_inverse) / (velocity * KM_H)})
-                        });
-                    }
-                }
-            }
+	    [this, &stops_graph, &catalogue](const auto& num_and_data){
+	            const auto& bus_data = num_and_data.second;
+	            const auto& stops = bus_data.get()->stop_ptrs;
+	            size_t stops_count = stops.size();
+	            for (size_t i = 0; i < stops_count; ++i) {
+	                for (size_t j = i + 1; j < stops_count; ++j) {
+	                
+	                    const std::shared_ptr<Stop> from = stops[i];
+	                    const std::shared_ptr<Stop> to = stops[j];
+	                    
+	                    int dist_sum = 0;
+	                    int dist_sum_inverse = 0;
+	                    
+	                    for (size_t x = i + 1; x <= j; ++x) {
+	                        dist_sum += catalogue.GetDistance(stops[x - 1].get(), stops[x].get());
+	                        dist_sum_inverse += catalogue.GetDistance(stops[x].get(), stops[x - 1].get());
+	                      
+	                    }
+	                    
+	                    stops_graph.AddEdge({
+	                    stops_to_points.at(from.get()->name) + 1,
+	                    stops_to_points.at(to.get()->name),
+	                    
+	                    EdgeData({bus_data.get()->num, (int)(j-i), (double)(dist_sum) / (velocity * KM_H)})
+	                    });    
+	                    if (bus_data.get()->type == RouteType::LINEAR) {
+	                        stops_graph.AddEdge({
+	                        stops_to_points.at(to.get()->name) + 1,
+	                        stops_to_points.at(from.get()->name),
+	                        
+	                        EdgeData({bus_data.get()->num, (int)(j-i), (double)(dist_sum_inverse) / (velocity * KM_H)})
+	                        });
+	                    }
+	              }
+	        }
         });
 
     graph = std::make_shared<Graph>(stops_graph);
